acacb4118022a4d1b23a7b7d6a971aaa3ac51688
Saurabh Sutar <saurahbsutar192@gmail.com>
2021-08-12 16:47:33 +0530

    Portfolio v1

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,44 +8,42 @@ import { Link } from "gatsby"
 
 function Layout({ children }) {
   return (
-    <>
-      <div className="layoutContainer">
-        <div className="NavbarContainer">
-          <div className="logo">SS</div>
-          <div className="navs">
-            <Link to="/">
-              <span>About</span>
-            </Link>
-            <Link to="/projects">
-              <span>Projects</span>
-            </Link>
-            <Link to="/blogs">
-              <span>Blogs</span>
-            </Link>
-          </div>
+    <div className="layoutContainer">
+      <div className="NavbarContainer">
+        <div className="logo">SS</div>
+        <div className="navs">
+          <Link to="/">
+            <span>About</span>
+          </Link>
+          <Link to="/projects">
+            <span>Projects</span>
+          </Link>
+          <Link to="/blogs">
+            <span>Blogs</span>
+          </Link>
         </div>
-        <main>{children}</main>
-        <footer className="links">
-          <a href="https://github.com/" target="_blank" rel="noreferrer">
-            <GitHubIcon />
-          </a>
-          <a
-            href="https://www.instagram.com/_saurabh__sutar_"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <InstagramIcon />
-          </a>
-          <a
-            href="https://www.facebook.com/saurabh.sutar.12"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FacebookIcon />
-          </a>
-        </footer>
       </div>
-    </>
+      <main>{children}</main>
+      <footer className="links">
+        <a href="https://github.com/" target="_blank" rel="noreferrer">
+          <GitHubIcon />
+        </a>
+        <a
+          href="https://www.instagram.com/_saurabh__sutar_"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <InstagramIcon />
+        </a>
+        <a
+          href="https://www.facebook.com/saurabh.sutar.12"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FacebookIcon />
+        </a>
+      </footer>
+    </div>
   )
 }
 
