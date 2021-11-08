376287d7c99b826b7a317e60a954ad3685b59937
saurabhsutar192 <81956858+saurabhsutar192@users.noreply.github.com>
2021-11-08 16:30:40 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,10 @@ function About() {
   return (
     <div className="aboutContainer">
       <div className="about">
-        <div>About</div>I am currently working as a front-end Intern at
+        <div>About</div>I am currently working as a front-end Dev at
         <a href="https://www.antstack.io/" target="_blank" rel="noreferrer">
           {" "}
-          AntStack Technologies, Bangaluru
+          AntStack Technologies, Bangalore
         </a>
         , where my passion in development converge. Apart from work I am a
         musician. I play piano and I also love to game on my PC
